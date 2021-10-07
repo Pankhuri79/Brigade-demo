30915f227588ef819a57445ffee77f49db7a7244
PANKHURI KUMARI <89061186+Pankhuri79@users.noreply.github.com>
2021-10-07 15:56:54 +0530

    Create brigade.js

---
+++ brigade.js
@@ -0,0 +1,22 @@
+const { events, Job } = require("brigadier");
+events.on("exec", (e,project) => {
+  var dockerBuild = new Job("docker-packaging");
+  dockerBuild.image = "docker:dind";
+  dockerBuild.privileged = true;
+  dockerBuild.env = {
+    DOCKER_DRIVER: "overlay",
+    DOCKER_USER: project.secrets.dockerLogin,
+    DOCKER_PASS: project.secrets.dockerPass
+  }
+    
+  dockerBuild.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 30",
+    "cd /src",
+    "docker build -t Pankhuri79/demo:04 .",
+    "docker login -u $DOCKER_USER -p $DOCKER_PASS",
+    "docker push Pankhuri79/demo:04"
+  ];
+  
+  dockerBuild.run();
+});
